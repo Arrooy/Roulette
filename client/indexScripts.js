5d89d683cecae6303b6797d2956626ad1db19cb7
HobJob <adria.arroyo.2016@salleurl.edu>
2017-06-16 18:13:24 +0200

    Falta hacer update del server, administrar el socket SignUp

--- client/indexScripts.js
+++ client/indexScripts.js
@@ -9,18 +9,120 @@
 	var ctx = document.getElementById("ctx").getContext("2d");
 	var ctxUi = document.getElementById("ctx-ui").getContext("2d");
 
+	var Fase1 = document.getElementById('CrearCuenta1');
+	var Fase2 = document.getElementById('CrearCuenta2');
+	var NextStep1 = document.getElementById('Step1');
 
+		var CreateAccount = document.getElementById('CA');
+
+			var ReHOme = document.getElementById('Step0');
 
 	var ErrorType ;
 	var ChatOpened;
 	var adminColor = false;
 
+	var CEmail = document.getElementById('CEmail');
 
+	var CUsername = document.getElementById('CUsername');
+
+	var CColor = document.getElementById('CColor');
+
+	var CAge = document.getElementById('CAge');
+
+
+	var CPassword1 = document.getElementById('CPassword1');
+
+	var CPassword2 = document.getElementById('CPassword2');
+
+	ReHOme.onclick = function(){
+		Fase1.style.display = '';
+		Fase2.style.display = 'none';
+		signDiv.style.display = 'none';
+	}
+
+	NextStep1.onclick =function(){
+		var NoError = 1;
+		var data = CAge.value.split('-');
+		var UserAge = new Date();
+		UserAge.setFullYear(data[0],data[1]-1,data[2]);
+		var LimitedAge = new Date();
+		LimitedAge.setFullYear(1999,11,31);
+		var countError = 0;
+		if(CEmail.value.indexOf('@') == -1){
+			NoError = 0;
+			ErrorType = 3;
+			$("#CEmailLabel").css('color','#F05941');
+			$('#alerta').modal('toggle')
+		}else{
+			$("#CEmailLabel").css('color','#2F1B41');
+		}
+		if(NoError == 1){
+			if (data[0] == "" || data[1] == undefined || data[2] == undefined){
+				NoError = 0;
+				countError = 1;
+				$("#LabelCAge").css('color','#F05941');
+			}else{
+				$("#LabelCAge").css('color','#2F1B41');
+			}
+		}
+		if(NoError == 1){
+			if(UserAge > LimitedAge){
+				NoError = 0;
+				ErrorType = 4;
+				$("#LabelCAge").css('color','#F05941');
+				$('#alerta').modal('toggle')
+			}else{
+				$("#LabelCAge").css('color','#2F1B41');
+			}
+		}
+		if(NoError === 1){
+
+			Fase1.style.display = 'none';
+			Fase2.style.display = '';
+			signDiv.style.display = 'none';
+		}
+
+	}
+	CreateAccount.onclick = function(){
+		var error = 0;
+		if(CPassword1.value == ""){
+			$("#Pass1").css('color','#F05941');
+			error = 1;
+		}else{
+			$("#Pass1").css('color','#2F1B41');
+		}
+
+		if(CPassword2.value == ""){
+			$("#Pass2").css('color','#F05941');
+			error = 1;
+		}else{
+			$("#Pass2").css('color','#2F1B41');
+		}
+		if(CPassword1.value != CPassword2.value){
+			error = 1;
+			ErrorType = 5;
+			$('#alerta').modal('toggle')
+		}
+		if(error == 0){
+			socket.emit('signUp',{
+				email:CEmail.value,
+				username:CUsername.value,
+				age:CAge.value,
+				color:CColor.value,
+				password:CPassword1.value
+			});
+		}
+
+
+	}
 	signDivSignIn.onclick = function(){
 		socket.emit('signIn',{username:signDivUsername.value,password:signDivPassword.value});
 	}
 	signDivSignUp.onclick = function(){
-		socket.emit('signUp',{username:signDivUsername.value,password:signDivPassword.value});
+		Fase1.style.display = '';
+		Fase2.style.display = 'none';
+		signDiv.style.display = 'none';
+
 	}
 	socket.on('signInResponse',function(data){
 		if(data.success){
@@ -42,25 +144,45 @@
 	socket.on('signUpResponse',function(data){
 		if(data.success){
 
+			Fase1.style.display = 'none';
+			Fase2.style.display = 'none';
+			signDiv.style.display = '';
+			$("#CUsernameLabel").css('color','#2F1B41');
 		}else{
 			ErrorType = 1;
+			$("#CUsernameLabel").css('color','#F05941');
 			$('#alerta').modal('toggle')
 		}
 	});
 
 	$('#alerta').on('show.bs.modal', function (event) {
   var modal = $(this)
-	console.log(ErrorType);
+
 	switch (ErrorType) {
 		case 1://Error signUp
 		modal.find('#tituloModal').text('This is weird...')
-		modal.find('#textoModal').text('We cant handle your account, try again in about 3 minutes.')
+		modal.find('#textoModal').text('Your account Username is taken! try again')
+		Fase1.style.display = '';
+		Fase2.style.display = 'none';
+		signDiv.style.display = 'none';
 			break;
 
 			case 2://Error signUp
 			modal.find('#tituloModal').text('Well...')
 			modal.find('#textoModal').text('Combination wrong. Feel free to try again!')
 				break;
+				case 3://Error signUp
+				modal.find('#tituloModal').text('Email error!')
+				modal.find('#textoModal').text('What about using a correct E-MAIL')
+					break;
+					case 4://Error signUp
+					modal.find('#tituloModal').text('Too little: ')
+					modal.find('#textoModal').text('This game ROULES doesnt let you come in. You must be 18 to play.')
+						break;
+						case 5://Error signUp
+						modal.find('#tituloModal').text('Passwords arent the same')
+						modal.find('#textoModal').text('Try to insert the SAME password in both fields.')
+							break;
 		default:
 		modal.find('#tituloModal').text('OH')
 		modal.find('#textoModal').text('Our tech group failed, report us')
@@ -154,18 +276,16 @@
 		Img.cursor.src = '/client/img/'+self.cur+'.png';
 
 		self.draw = function(){
-		if(self.cur != undefined){
-				ctx.drawImage(Img.cursor,0,0,Img.cursor.width,Img.cursor.height,self.x-Img.cursor.width/2,self.y-Img.cursor.height/2,Img.cursor.width,Img.cursor.height);
-		}
 
 
-			if(selfId != self.id)
+
+			if(selfId != self.id){
+				if(self.cur != undefined){
+						ctx.drawImage(Img.cursor,0,0,Img.cursor.width,Img.cursor.height,self.x-Img.cursor.width/2,self.y-Img.cursor.height/2,Img.cursor.width,Img.cursor.height);
+				}
 				return;
-			var xp = self.xp;
-			ctx.fillStyle = 'black';
-			ctx.fillRect(0,window.innerHeight - 15,window.innerWidth - 505,12);
-			ctx.fillStyle = 'green';
-			ctx.fillRect(0,window.innerHeight - 15,(window.innerWidth - 505)*(self.xp/(10*self.lvl)),12);
+			}
+
 		}
 
 		Player.list[self.id] = self;
