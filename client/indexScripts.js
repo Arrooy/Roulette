2424dd8a9f1b1631a88065eaae571bfd413f1a27
Adria Arroyo Martinez <adria.arroyo.2016@salleurl.edu>
2017-07-13 13:52:41 +0100

    uy

--- client/indexScripts.js
+++ client/indexScripts.js
@@ -53,13 +53,13 @@
 
 
 
-	 GoInicio.onclick = function() {
-		Fase1.style.display = 'none';
- 	  Fase2.style.display = 'none';
- 	  signDiv.style.display = '';
-		$("#ContainerMail").removeClass('animated infinite  ' + ErrorAnimation);
+	GoInicio.onclick = function() {
+	  Fase1.style.display = 'none';
+	  Fase2.style.display = 'none';
+	  signDiv.style.display = '';
+	  $("#ContainerMail").removeClass('animated infinite  ' + ErrorAnimation);
 	  $("#CAgediv").removeClass('animated infinite  ' + ErrorAnimation);
-	 }
+	}
 
 	ReHOme.onclick = function() {
 	  Fase1.style.display = '';
@@ -293,7 +293,7 @@
 	})
 
 	//chat
-var Chat = new chat;
+	var Chat = new chat;
 
 
 	var Img = {};
@@ -336,43 +336,30 @@ var Chat = new chat;
 	  self.x = initPack.x;
 	  self.y = initPack.y;
 	  Img.cursor.src = '/client/img/' + self.cur + '.png';
-		Img.minero.src = '/client/img/Miner.png';
-		Img.mine.src = '/client/img/mine.png';
-		Img.explode.src = '/client/img/explosion.png';
-		Img.minerIco.src = '/client/img/minerIco.png';
-		Img.groundBasic.src = '/client/img/grass.png';
-		Img.limit.src = '/client/img/Limits.png';
+	  Img.minero.src = '/client/img/Miner.png';
+	  Img.mine.src = '/client/img/mine.png';
+	  Img.explode.src = '/client/img/explosion.png';
+	  Img.minerIco.src = '/client/img/minerIco.png';
+	  Img.groundBasic.src = '/client/img/grass.png';
+	  Img.limit.src = '/client/img/Limits.png';
 
 
 	  self.draw = function() {
 
-			var AnimationSpeed = 100;
+	    var AnimationSpeed = 100;
 	    if (selfId != self.id) {
 	      if (self.cur != undefined) {
 
-						//rotarImagen("ctx",Img.minero,0,self.x,self.y,"CENTER","SpriteVertical",0,i*72,72,72);
-						//rotarImagen("ctx",Img.mine,0,self.x,self.y,"CENTER","SpriteVertical",0,i*96,96,96);
-					//rotarImagen("ctx",Img.explode,0,self.x,self.y,"CENTER","SpriteVertical",i*64,j * 64,64,64);
-						//rotarImagen("ctx",Img.minerIco,0,self.x,self.y,"CENTER");
-						//rotarImagen("ctx",Img.groundBasic,0,self.x,self.y,"CENTER");
-						//rotarImagen("ctx",knight[i],0,j*20,self.y,"CENTER");
+	        //rotarImagen("ctx",Img.minero,0,self.x,self.y,"CENTER","SpriteVertical",0,i*72,72,72);
+	        //rotarImagen("ctx",Img.mine,0,self.x,self.y,"CENTER","SpriteVertical",0,i*96,96,96);
+	        //rotarImagen("ctx",Img.explode,0,self.x,self.y,"CENTER","SpriteVertical",i*64,j * 64,64,64);
+	        //rotarImagen("ctx",Img.minerIco,0,self.x,self.y,"CENTER");
+	        //rotarImagen("ctx",Img.groundBasic,0,self.x,self.y,"CENTER");
+	        //rotarImagen("ctx",knight[i],0,j*20,self.y,"CENTER");
 
-						curTime = +new Date();
-						if(curTime - prevTime >= AnimationSpeed){
-							i++;
-							prevTime = curTime;
-						}
-						if(i == 8){
-							i = 0;
-							j++;
-						}
-
-						if(j == 3){
-							j=0;
-						}
 	      }
 	      return;
-	   }
+	    }
 	  }
 
 	  Player.list[self.id] = self;
@@ -430,20 +417,16 @@ var Chat = new chat;
 	var millis = 0;
 	var lastMillis = 0;
 
-	Mines = new buildMines(0,0,12,40,40);
+	Mines = new buildMines(0, 0, 12, 40, 40);
 
-			var intento = 20;
-			do{
-				var result = window.innerWidth / intento;
-				error = Distancia();
-				intento+=0.1;
-			}while(error > 1.5)
-			console.log(intento);
+	var Resolution = window.innerWidth / window.innerHeight;
 
-
+	$(window).resize(function() {
+	  location.reload();
+	});
 
 	setInterval(function() {
-		millis = +new Date();
+	  millis = +new Date();
 	  if (adminColor === true) {
 	    var a = document.getElementsByName('admin');
 	    var i = 0;
@@ -451,7 +434,7 @@ var Chat = new chat;
 	      a[i].style.color = '#' + (0x1000000 + (Math.random()) * 0xffffff).toString(16).substr(1, 6);
 	    }
 	  }
-		console.log(window.innerWidth + " " + window.innerHeight);
+	  console.log(window.innerWidth + " " + window.innerHeight);
 
 	  if (!selfId)
 	    return;
@@ -460,39 +443,40 @@ var Chat = new chat;
 	  ctx.clearRect(0, 0, document.getElementById('ctx').width, document.getElementById('ctx').height);
 
 
-    //Terrain
-		for(var x = -1;x < window.innerWidth / Img.groundBasic.width ;x++){
-			for(var y = -1;y < window.innerHeight / Img.groundBasic.height ;y++){
-				ctx.drawImage(Img.groundBasic,x*Img.groundBasic.width,y*Img.groundBasic.height, Img.groundBasic.width+1, Img.groundBasic.height+1);
-			}
-		}
+	  //Terrain
+	  for (var x = -1; x < window.innerWidth / Img.groundBasic.width; x++) {
+	    for (var y = -1; y < window.innerHeight / Img.groundBasic.height; y++) {
+	      ctx.drawImage(Img.groundBasic, x * Img.groundBasic.width, y * Img.groundBasic.height, Img.groundBasic.width + 1, Img.groundBasic.height + 1);
+	    }
+	  }
 
 
-		for(var x = 0;x < window.innerWidth / intento ;x++){
-			//ctx.drawImage(Img.limit,x*Img.limit.width,0, Img.limit.width, Img.limit.height);
-			rotarImagen("ctx",Img.limit,0,intento/2 + x*intento,150,"leftUp","SpriteVertical",0,0,64,64,intento,intento);
-		}/*
-		for(var x = 0;x < window.innerWidth / Img.limit.width ;x++){
-			ctx.drawImage(Img.limit,x*Img.limit.width,window.innerHeight - Img.limit.height/2, Img.limit.width, Img.limit.height);
-		}
-		for(var y = 0;y < window.innerHeight / Img.limit.height ;y++){
-			ctx.drawImage(Img.limit,Img.limit.width,Img.limit.height * y, Img.limit.width, Img.limit.height);
-		}
-		for(var y = 0;y < window.innerHeight / Img.limit.height ;y++){
-			ctx.drawImage(Img.limit,window.innerWidth - Img.limit.width/2,Img.limit.height * y, Img.limit.width, Img.limit.height);
-		}
-*/
+	  for (var x = 0; x < window.innerWidth / intento; x++) {
+	    //ctx.drawImage(Img.limit,x*Img.limit.width,0, Img.limit.width, Img.limit.height);
+	    rotarImagen("ctx", Img.limit, 0, intento / 2 + x * intento, 150, "leftUp", "SpriteVertical", 0, 0, 64, 64, intento, intento);
+	  }
+	  /*
+	  		for(var x = 0;x < window.innerWidth / Img.limit.width ;x++){
+	  			ctx.drawImage(Img.limit,x*Img.limit.width,window.innerHeight - Img.limit.height/2, Img.limit.width, Img.limit.height);
+	  		}
+	  		for(var y = 0;y < window.innerHeight / Img.limit.height ;y++){
+	  			ctx.drawImage(Img.limit,Img.limit.width,Img.limit.height * y, Img.limit.width, Img.limit.height);
+	  		}
+	  		for(var y = 0;y < window.innerHeight / Img.limit.height ;y++){
+	  			ctx.drawImage(Img.limit,window.innerWidth - Img.limit.width/2,Img.limit.height * y, Img.limit.width, Img.limit.height);
+	  		}
+	  */
 
 
-    //Minas
-		for(var x = 0;x < Mines.numeroMinas;x++){
-			rotarImagen("ctx",Img.mine,0,Mines.Minax[x],Mines.Minay[x],"CENTER","SpriteVertical",0,Mines.FrameMina[x]*96,96,96,Mines.widthMina,Mines.heightMina);
-			FrameMina = Math.round(Math.random()*1);
-		}
+	  //Minas
+	  for (var x = 0; x < Mines.numeroMinas; x++) {
+	    rotarImagen("ctx", Img.mine, 0, Mines.Minax[x], Mines.Minay[x], "CENTER", "SpriteVertical", 0, Mines.FrameMina[x] * 96, 96, 96, Mines.widthMina, Mines.heightMina);
+	    FrameMina = Math.round(Math.random() * 1);
+	  }
 
 
-		for (var i in Player.list)
-			Player.list[i].draw();
+	  for (var i in Player.list)
+	    Player.list[i].draw();
 
 	}, 40);
 
@@ -537,76 +521,76 @@ var Chat = new chat;
 	  event.preventDefault();
 	}
 
-	rotarImagen = function(NombreCanvas,Img,angle,x,y,Locations,Type,initx,inity,width,height,desiredSizex,desiredSizey){
-		angle = angle * Math.PI / 180;
-		if(Type === "SpriteVertical"){
-			ctxx = document.getElementById(NombreCanvas).getContext("2d");
-			ctxx.save();
-			ctxx.translate(x, y);
-			ctxx.rotate(angle);
-			if(desiredSizey == undefined){
-				ctxx.drawImage(Img,initx,inity,width,height,width / -2,height / -2,width,height);
-			}else{
-				ctxx.drawImage(Img,initx,inity,width,height,width / -2,height / -2,desiredSizex,desiredSizey);
-			}
+	rotarImagen = function(NombreCanvas, Img, angle, x, y, Locations, Type, initx, inity, width, height, desiredSizex, desiredSizey) {
+	  angle = angle * Math.PI / 180;
+	  if (Type === "SpriteVertical") {
+	    ctxx = document.getElementById(NombreCanvas).getContext("2d");
+	    ctxx.save();
+	    ctxx.translate(x, y);
+	    ctxx.rotate(angle);
+	    if (desiredSizey == undefined) {
+	      ctxx.drawImage(Img, initx, inity, width, height, width / -2, height / -2, width, height);
+	    } else {
+	      ctxx.drawImage(Img, initx, inity, width, height, width / -2, height / -2, desiredSizex, desiredSizey);
+	    }
 
-			ctxx.restore();
-		}else{
-			if(Locations === "center" ||Locations === "Center"||Locations === "CENTER"){
-				ctxx = document.getElementById(NombreCanvas).getContext("2d");
-				ctxx.save();
-				ctxx.translate(x, y);
-				ctxx.rotate(angle);
-				if(width == undefined ||height == undefined){
-					ctxx.drawImage(Img,Img.width / -2,Img.height / -2, Img.width, Img.height);
-				}else{
-					ctxx.drawImage(Img,width / -2,height / -2, width, height);
-				}
-				ctxx.restore();
-			}else if(Locations === "leftUp"||Locations === "left-up"||Locations === "leftup"||Locations === "LeftUp"||Locations === "LEFTUP"){
-				ctxx = document.getElementById(NombreCanvas).getContext("2d");
-				ctxx.save();
-				ctxx.translate(x - Img.width / 2 , y - Img.height / 2);
-				ctxx.rotate(angle);
-				ctxx.drawImage(Img,Img.width / 2, Img.height / 2, Img.width, Img.height);
-				ctxx.restore();
-			}
-		}
+	    ctxx.restore();
+	  } else {
+	    if (Locations === "center" || Locations === "Center" || Locations === "CENTER") {
+	      ctxx = document.getElementById(NombreCanvas).getContext("2d");
+	      ctxx.save();
+	      ctxx.translate(x, y);
+	      ctxx.rotate(angle);
+	      if (width == undefined || height == undefined) {
+	        ctxx.drawImage(Img, Img.width / -2, Img.height / -2, Img.width, Img.height);
+	      } else {
+	        ctxx.drawImage(Img, width / -2, height / -2, width, height);
+	      }
+	      ctxx.restore();
+	    } else if (Locations === "leftUp" || Locations === "left-up" || Locations === "leftup" || Locations === "LeftUp" || Locations === "LEFTUP") {
+	      ctxx = document.getElementById(NombreCanvas).getContext("2d");
+	      ctxx.save();
+	      ctxx.translate(x - Img.width / 2, y - Img.height / 2);
+	      ctxx.rotate(angle);
+	      ctxx.drawImage(Img, Img.width / 2, Img.height / 2, Img.width, Img.height);
+	      ctxx.restore();
+	    }
+	  }
 	}
 
-OrientadoA = function(Px,Py,Ox,Oy,Quadrante){
+	OrientadoA = function(Px, Py, Ox, Oy, Quadrante) {
 
-	var AB =  Vector.Create(Px,Py,Ox,Oy);
-var Neutro;
-	if(Quadrante === 0){
-	  Neutro = Vector.Create(0,0,1,0);
-	}else if(Quadrante ===1 ){
-		Neutro = Vector.Create(1,0,0,0);
-	}else if(Quadrante ===2 ){
-		Neutro = Vector.Create(0,0,1,0);
-	}else if(Quadrante ===3 ){
-		Neutro = Vector.Create(1,0,0,0);
+	  var AB = Vector.Create(Px, Py, Ox, Oy);
+	  var Neutro;
+	  if (Quadrante === 0) {
+	    Neutro = Vector.Create(0, 0, 1, 0);
+	  } else if (Quadrante === 1) {
+	    Neutro = Vector.Create(1, 0, 0, 0);
+	  } else if (Quadrante === 2) {
+	    Neutro = Vector.Create(0, 0, 1, 0);
+	  } else if (Quadrante === 3) {
+	    Neutro = Vector.Create(1, 0, 0, 0);
+	  }
+
+	  var jeje = Vector.Angle(AB, Neutro);
+	  return jeje / Math.PI * 180;
 	}
 
-	var jeje = Vector.Angle(AB,Neutro);
-	return jeje / Math.PI*180;
-}
-
-Distancia = function(Px,Py,Ox,Oy){
-	return (Math.sqrt(Math.pow((Px-Ox),2)+Math.pow((Py-Oy),2)));
-}
-
-var Vector = {
-	"Create": function(Px,Py,Ox,Oy){
-		return [Math.abs(-Px+Ox),Math.abs(-Py+Oy)];
-	},
-	"Angle": function(vec1,vec2){
-		return Math.acos( (Vector.ProducteEscalar(vec1,vec2)) / ( Vector.Modulo(vec1) * Vector.Modulo(vec2) ) );
-	},
-	"Modulo": function(array){
-		return Math.sqrt(Math.pow(array[0],2)+Math.pow(array[1],2));
-	},
-	"ProducteEscalar":function(vec1,vec2){
-		return (vec1[0]*vec2[0] + vec1[1]*vec2[1]);
+	Distancia = function(Px, Py, Ox, Oy) {
+	  return (Math.sqrt(Math.pow((Px - Ox), 2) + Math.pow((Py - Oy), 2)));
 	}
-}
+
+	var Vector = {
+	  "Create": function(Px, Py, Ox, Oy) {
+	    return [Math.abs(-Px + Ox), Math.abs(-Py + Oy)];
+	  },
+	  "Angle": function(vec1, vec2) {
+	    return Math.acos((Vector.ProducteEscalar(vec1, vec2)) / (Vector.Modulo(vec1) * Vector.Modulo(vec2)));
+	  },
+	  "Modulo": function(array) {
+	    return Math.sqrt(Math.pow(array[0], 2) + Math.pow(array[1], 2));
+	  },
+	  "ProducteEscalar": function(vec1, vec2) {
+	    return (vec1[0] * vec2[0] + vec1[1] * vec2[1]);
+	  }
+	}
