e23321d64884e58d980c23bee0a80a3527aeece5
Adria Arroyo Martinez <adria.arroyo.2016@salleurl.edu>
2017-08-23 10:43:01 +0100

    sd

--- client/main.js
+++ client/main.js
@@ -1,3 +1,18 @@
+var lifetime; // How long should each generation live
+
+var population; // Population
+
+var lifecycle; // Timer for cycle of generation
+var recordtime; // Fastest time to target
+
+var target; // Target position
+
+//var diam = 24;          // Size of target
+
+var obstacles = []; //an array list to keep track of all the obstacles!
+
+var setUp = 1;
+
 var Player = function(initPack) {
   var self = {};
   self.id = initPack.id;
@@ -74,17 +89,74 @@ $(window).resize(function() {
 
 setInterval(function() {
 
-  //Public stuff
+  if (setUp) {
+    lifetime = 300;
+
+    // Initialize variables
+    lifecycle = 0;
+    recordtime = lifetime;
+
+    target = new Obstacle(width / 2 - 12, 24, 24, 24);
+
+    // Create a population with a mutation rate, and population max
+    var mutationRate = 0.01;
+    population = new Population(mutationRate, 50);
+
+    // Create the obstacle course
+    obstacles = [];
+    obstacles.push(new Obstacle(width / 2 - 100, height / 2, 200, 10));
+    setUp = 0;
+  } else {
 
 
-  if (!selfId)
-    return;
 
-  //Private stuff
-  //Update canvas
-  ctx.clearRect(0, 0, $("#GameCanvas").css("width"), $("#GameCanvas").css("height"));
+    target.display();
+
+
+    // If the generation hasn't ended yet
+    if (lifecycle < lifetime) {
+      population.live(obstacles);
+      if ((population.targetReached()) && (lifecycle < recordtime)) {
+        recordtime = lifecycle;
+      }
+      lifecycle++;
+      // Otherwise a new generation
+    } else {
+      lifecycle = 0;
+      population.fitness();
+      population.selection();
+      population.reproduction();
+    }
+
+    // Draw the obstacles
+    for (var i = 0; i < obstacles.length; i++) {
+      obstacles[i].display();
+    }
+
+
+    ctx.fillText("Generation #: " + population.getGenerations(), 10, 18);
+    ctx.fillText("Cycles left: " + (lifetime - lifecycle), 10, 36);
+    ctx.fillText("Record cycles: " + recordtime, 10, 54);
+    //Public stuff
+
+
+    if (!selfId)
+      return;
+
+    //Private stuff
+    //Update canvas
+    ctx.clearRect(0, 0, $("#GameCanvas").css("width"), $("#GameCanvas").css("height"));
+  }
 
   for (var i in Player.list)
     Player.list[i].draw();
 
 }, 40);
+
+
+
+document.addEventListener("click", function() {
+  target.position.x = mouseX;
+  target.position.y = mouseY;
+  recordtime = lifetime;
+});
