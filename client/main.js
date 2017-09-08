bd8368dcbe63b9bbd296ece7b556c3dc5f7509cd
Adria Arroyo Martinez <adria.arroyo.2016@salleurl.edu>
2017-09-08 20:14:39 +0100

    s

--- client/main.js
+++ client/main.js
@@ -111,13 +111,8 @@ setInterval(function() {
     //indicador(window.innerWidth / 2, window.innerHeight / 2, as += 10, 350, 10);
 
     indicador(Player.list[selfId].x, Player.list[selfId].y, as += 1, (350 * ctx.canvas.width) / OrignialSizeX, (10 * ctx.canvas.height) / OrignialSizeY);
-    var masPeke;
-    if (ctx.canvas.width < ctx.canvas.height) {
-      masPeke = ctx.canvas.width;
-    } else {
-      masPeke = ctx.canvas.height;
-    }
-    ctx.drawImage(Ruleta16, Player.list[selfId].x - ((420 * masPeke) / OrignialSizeX) / 2, Player.list[selfId].y - ((420 * masPeke) / OrignialSizeY) / 2, (420 * masPeke) / OrignialSizeX, (420 * masPeke) / OrignialSizeY);
+
+    ctx.drawImage(Ruleta16, Player.list[selfId].x - ((420 * ctx.canvas.width) / OrignialSizeX) / 2, Player.list[selfId].y - ((420 * ctx.canvas.height) / OrignialSizeY) / 2, (420 * ctx.canvas.width) / OrignialSizeX, (420 * ctx.canvas.height) / OrignialSizeY);
   }
 }, 40);
 //JO = Player.list[selfId]
